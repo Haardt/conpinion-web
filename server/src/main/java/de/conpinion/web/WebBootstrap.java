7ac652ae3ff88a26d5557b2da996ccef4606c9b8
Andreas Haardt <andreas@iritgo.de>
2016-10-01 01:00:58 +0200

    mod .gitignore

--- server/src/main/java/de/conpinion/web/WebBootstrap.java
+++ server/src/main/java/de/conpinion/web/WebBootstrap.java
@@ -5,24 +5,31 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import java.util.function.BiFunction;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class WebBootstrap extends AbstractVerticle
-{
-	public static void main(String[] args)
-	{
-		log.info("web-comfort starting...");
-		Vertx vertx = Vertx.vertx();
+public class WebBootstrap extends AbstractVerticle {
+    
+    public BiFunction<Integer, Integer, Integer> add = (x,y) -> x + y;
+    public BiFunction<Integer, Integer, Integer> add5 = add.andThen(x -> x * 5);
+    
+    
 
-		DeploymentOptions webServerOptions = new DeploymentOptions();
-		vertx.deployVerticle(WebServerVerticle.class.getName(), webServerOptions);
-	}
+    public static void main(String[] args) {
 
-	@Override
-	public void start(Future<Void> startFuture) throws Exception
-	{
-		DeploymentOptions webServerOptions = new DeploymentOptions();
-		vertx.deployVerticle(WebServerVerticle.class.getName(), webServerOptions);
-	}
+        Vertx vertx = Vertx.vertx();
+
+        DeploymentOptions webServerOptions = new DeploymentOptions();
+        vertx.deployVerticle(WebServerVerticle.class.getName(), webServerOptions);
+    }
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        Integer apply = new WebBootstrap().add5.apply(2, 5);
+        log.info("web-comfort starting...{}", apply);
+
+        DeploymentOptions webServerOptions = new DeploymentOptions();
+        vertx.deployVerticle(WebServerVerticle.class.getName(), webServerOptions);
+    }
 }
